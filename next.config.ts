ac6e1e6b4c4463a88df58979328edf3c004da5b6
KenRambo88 <6509457+KenRambo@users.noreply.github.com>
2025-04-18 17:02:27 -0400

    disabling linting for prod hotfix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // next.config.js
-module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
-  /* config options here */
+  // other config options here
 };
 
 export default nextConfig;
